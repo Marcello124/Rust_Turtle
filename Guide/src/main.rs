470c14dac39cc923c53fe8293263e238441741ba
Marcel <marceltrzaskawka129@gmail.com>
2023-02-21 00:45:54 +0100

    Created repo, massive push

---
+++ Guide/src/main.rs
@@ -0,0 +1,67 @@
+use turtle::Turtle;
+
+fn main() {
+    let mut turtle = Turtle::new();
+    turtle.set_speed(18);
+  
+    
+    // introduction
+
+
+    // example
+    // turtle.forward(100.0);
+    // for _ in 0..360 {
+    //     turtle.forward(3.0);
+    //     turtle.right(1.0);
+    // }
+
+    // exercise 1
+    // turtle.forward(100.0);
+    // turtle.right(90.0);
+    // turtle.forward(100.0);
+
+    // exercise 2
+    // turtle.forward(100.0);
+    // turtle.right(270.0);
+    // turtle.forward(150.0);
+
+    // exercise 3
+    // turtle.right(180.0);
+    // turtle.forward(100.0);
+    // turtle.right(-90.0); // goes left
+
+    // exercise 4
+    // turtle.forward(125.0);
+    // turtle.forward(125.0);
+
+    // exercise 5
+    // for _ in 0..4 {
+    //     turtle.forward(-100.0);
+    //     turtle.right(-90.0);
+    //     turtle.forward(100.0);
+    // }
+
+
+    // Basics
+
+
+    // Drawing a triangle
+    // for _ in 0..3 {
+    //     turtle.forward(100.0);
+    //     turtle.right(120.0);
+    // }
+
+    // Drawing 3 triangles 
+    // turtle.left(30.0);
+    // for _ in 0..3 {
+    //     for _ in 0..3 {
+    //         turtle.forward(100.0);
+    //         turtle.right(120.0);
+    //     }
+    //     turtle.right(120.0);
+    // }
+    // turtle.left(150.0);
+
+    // exercise 1
+
+}
